3ed0265de90a34d24293e4a8aff1d16e47cb8b9d
nayera540 <1900205@eng.asu.edu.eg>
2024-07-15 20:21:34 +0300

    Adding accodrion  items

--- faq-accordion-main/src/App.js
+++ faq-accordion-main/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const faqs = [
   {
     title: "What is Frontend Mentor, and how will it help me?",
@@ -67,13 +69,65 @@ function FaqsTitle() {
   );
 }
 
-function Accordion(){
-  return(<div className="accordion">
-    {}
-  </div>);
+function Accordion() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="accordion">
+      {faqs.map((el, i) => (
+        <AccordionItem title={el.title} onOpen={setIsOpen} isOpen={isOpen} key={el.title}>
+          {el.text}
+        </AccordionItem>
+      ))}
+    </div>
+  );
 }
 
-function AccordionItem() {}
+function AccordionItem({ title, children, onOpen, isOpen }) {
+  function handleToggle() {
+
+  }
+
+  return (
+    <div className={`accordion__item`} onClick={handleToggle}>
+      <p className="item__title">{title}</p>
+      <p className="item__icon">
+        {isOpen ? (
+          <span className="icon__open">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="31"
+              fill="none"
+              viewBox="0 0 30 31"
+            >
+              <path
+                fill="#301534"
+                d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"
+              />
+            </svg>
+          </span>
+        ) : (
+          <span className="icon__closed">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="31"
+              fill="none"
+              viewBox="0 0 30 31"
+            >
+              <path
+                fill="#AD28EB"
+                d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"
+              />
+            </svg>
+          </span>
+        )}
+      </p>
+      {isOpen && <div className="accordion__content">{children}</div>}
+    </div>
+  );
+}
 
 function Footer() {
   return (
