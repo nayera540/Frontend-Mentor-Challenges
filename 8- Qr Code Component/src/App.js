ebe0800fda305dae4a2eaa87128c2b8aad3cc0a2
nayera540 <1900205@eng.asu.edu.eg>
2024-08-17 00:53:32 +0300

    Add qr code project

---
+++ 8- Qr Code Component/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="qr__img">
+        <img src="/images/image-qr-code.png" alt="qr-code"></img>
+      </div>
+      <div className="header">
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
