340d97e6d96c657b83996f1596a3b19d1c8c5afa
nayera540 <1900205@eng.asu.edu.eg>
2024-10-05 15:49:43 +0300

    Initial commit

---
+++ 11- Stats Preview Card Component/Stats-Preview-Card/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+        colors:{
+            'trans--white-main': 'hsla(0, 0%, 100%, 0.75)',
+            'trans-white-stat': 'hsla(0, 0%, 100%, 0.6)',
+            'very-dark-blue': 'hsl(233, 47%, 7%)',
+            'dark-blue': 'hsl(244, 38%, 16%)',
+            'soft-violet': 'hsl(277, 64%, 61%)',
+        }
+    },
+  },
+  plugins: [],
+}
+
