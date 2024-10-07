3ac9ea103cffeeae73e9c7a2784b6e933d45bbb4
nayera540 <1900205@eng.asu.edu.eg>
2024-10-07 14:58:28 +0300

    Upload the project

--- 11- Stats Preview Card Component/Stats-Preview-Card/src/App.jsx
+++ 11- Stats Preview Card Component/Stats-Preview-Card/src/App.jsx
@@ -1,11 +1,33 @@
-
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <div className="container md:max-w-5xl max-w-lg grid grid-cols-1 md:grid-cols-2">
+            <div className="bg-dark-blue text-white md:p-10 p-4 order-2 md:order-1 text-center content-center justify-center md:justify-start md:text-start md:content-start rounded-l-md">
+                <h1 className="text-4xl my-7 font-bold font-inter">
+                    Get <span className="text-soft-violet">insights</span> that help your
+                    business grow.
+                </h1>
+                <p className="text-trans--white-main mb-14 font-lexand font-light text-base md:pr-10">
+                    Discover the benefits of data analytics and make better decisions
+                    regarding revenue, customer experience, and overall efficiency.
+                </p>
+                <div className="stats grid md:grid-cols-3">
+                    <div>
+                        10k+ <span>companies</span>
+                    </div>
+                    <div>
+                        314 <span>templates</span>
+                    </div>
+                    <div>
+                        12m+ <span>queries</span>
+                    </div>
+                </div>
+            </div>
+            <div className="relative order-1 md:order-2">
+                <img src="/images/image-header-desktop.jpg" className="rounded-r-md" />
+                <div className="absolute inset-0 bg-img-soft-violet bg-opacity-50"></div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- 11- Stats Preview Card Component/Stats-Preview-Card/tailwind.config.js
+++ 11- Stats Preview Card Component/Stats-Preview-Card/tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {
-        colors:{
-            'trans--white-main': 'hsla(0, 0%, 100%, 0.75)',
-            'trans-white-stat': 'hsla(0, 0%, 100%, 0.6)',
-            'very-dark-blue': 'hsl(233, 47%, 7%)',
-            'dark-blue': 'hsl(244, 38%, 16%)',
-            'soft-violet': 'hsl(277, 64%, 61%)',
-        }
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    theme: {
+        extend: {
+            fontSize:{
+                base: '15px',
+            },
+            fontFamily: {
+                inter: ['"Inter"', 'system-ui'],
+                lexand: ["Lexend Deca", "sans-serif"],
+            },
+            colors: {
+                "trans--white-main": "hsla(0, 0%, 100%, 0.75)",
+                "trans-white-stat": "hsla(0, 0%, 100%, 0.6)",
+                "very-dark-blue": "hsl(233, 47%, 7%)",
+                "dark-blue": "hsl(244, 38%, 16%)",
+                "soft-violet": "hsl(277, 64%, 61%)",
+                "img-soft-violet": "hsl(277deg 71.73% 37.22% / 52%);"
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
